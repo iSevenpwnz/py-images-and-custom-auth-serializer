946940203c0416ee33ed4913fe69ffe2eaca389a
Mykhailo Safronov <isevenpwnz@gmail.com>
2025-05-01 11:31:23 +0300

    solution

--- cinema/models.py
+++ cinema/models.py
@@ -1,6 +1,17 @@
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.conf import settings
+import uuid
+from django.utils.text import slugify
+import os
+
+
+def movie_image_file_path(instance, filename):
+    """Генерує шлях для зберігання зображення фільму"""
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads/movies/", filename)
 
 
 class CinemaHall(models.Model):
@@ -41,6 +52,7 @@ class Movie(models.Model):
     duration = models.IntegerField()
     genres = models.ManyToManyField(Genre)
     actors = models.ManyToManyField(Actor)
+    image = models.ImageField(null=True, upload_to=movie_image_file_path)
 
     class Meta:
         ordering = ["title"]
